42bc92b0fc4be0983e95dcc50b0ff97e27fa9957
Danial Dabirian <d.dabirian@jabama.com>
2023-11-23 12:47:58 +0330

    feat: init framework

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"github.com/gofiber/fiber/v2"
 	"go-upera-test/utils"
+	"log"
 )
 
 func main() {
-	utils.LoadConfig()
+	config, err := utils.LoadConfig()
+	if err != nil {
+		log.Fatalf("Could not load configs: %v", err)
+	}
+
+	app := fiber.New()
+
+	app.Listen(":" + config.Port)
 }
