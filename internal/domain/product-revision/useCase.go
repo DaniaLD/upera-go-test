cc8e0e3a899fc0022476b8b94c9928f1bcdf46db
Danial Dabirian <d.dabirian@jabama.com>
2023-11-24 19:13:06 +0330

    feat: product revision pagination

--- internal/domain/product-revision/useCase.go
+++ internal/domain/product-revision/useCase.go
@@ -4,6 +4,6 @@ import globalModel "github.com/DaniaLD/upera-go-test/pkg/model"
 
 type ProductRevisionUseCase interface {
 	Create(productId string, previousData globalModel.Product, newData globalModel.Product)
-	Get(productId string) (revisions []globalModel.ProductRevision, err error)
+	Get(productId string, limit int64, skip int64) (revisions []globalModel.ProductRevision, total int64, err error)
 	GetByRevisionId(productId string, revisionId int64) (product globalModel.Product, err error)
 }
